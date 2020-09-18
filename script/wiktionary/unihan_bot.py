15ee4e16f4eb972fac5991d3fdaf47d84914c561
szc126 <29127915+szc126@users.noreply.github.com>
2020-09-18 05:51:45 -0700

    unihan_bot.py: update

--- script/wiktionary/unihan_bot.py
+++ script/wiktionary/unihan_bot.py
@@ -8,54 +8,63 @@ from pywikibot.page import Page
 import re
 
 site = pywikibot.Site()
-gen = site.search('')
+gen = pywikibot.User(site, '350bot').contributions(namespaces = pywikibot.site.Namespace(0))
 
-# TODO:
-# interesting functions:
-# pywikibot.textlib.does_text_contain()
-# pywikibot.textlib.extract_sections()
+done = set()
 
-for page in gen:
+for page, revid, timestamp, summary in gen:
 	z = page.title()
+
+	if z in done:
+		continue
 	if len(z) != 1:
 		continue
-	try:
-		page.text = re.sub(r'{{character info}}\n', '', page.text)
 
+	try:
 		text_old = page.text
-		text_new = []
-		mul_text = unihan.newhzmul({
+
+		text_new = pywikibot.textlib.extract_sections(page.text)
+
+		text_mul = unihan.newhzmul({
 			'char': z,
 			'x': page.text,
 		})
-		mul_text = re.sub(r'(----)\n\n', '----', mul_text)
+		text_mul = pywikibot.textlib.extract_sections(text_mul)
 
-		if 'Translingual' in page.text:
-			b_add_lines = 1
-			for line in page.text.split('\n'):
-				if line == '==Translingual==':
-					b_add_lines = 0
-					text_new.append(mul_text)
-				elif b_add_lines == 0 and line == '----':
-					b_add_lines = 1
-				elif b_add_lines == 1:
-					text_new.append(line)
-			page.text = '\n'.join(text_new)
+		text_new_new = (text_new[0], text_mul[1], text_new[2])
 
-			print('==================')
+		if pywikibot.textlib.does_text_contain_section(page.text, 'Translingual'):
+			if not 'character info' in text_new_new[0]:
+				text_new_new[0] += text_mul[0]
+			bool_keep = True
+			for section in text_new[1]:
+				if 'Translingual' in section[0]:
+					bool_keep = False
+				elif re.search(r'^==[^=]', section[0]):
+					bool_keep = True
+				if bool_keep == True:
+					text_new_new[1].append(section)
+
+			page.text = text_new_new[0] + ''.join(header + body for header, body in text_new_new[1]) + text_new_new[2]
+
+			print('◆')
 			print(page.text)
-			print('==================')
+			print('◆')
 
 			pywikibot.showDiff(text_old, page.text)
 			input('[press enter to continue]')
 			page.save('/* Translingual */ rewrite')
 		else:
-			page.text = mul_text + page.text
+			if not 'character info' in text_new_new[0]:
+				text_new_new[0] += text_mul[0]
+			text_new_new[1].insert(0, text_mul[1][0])
+
+			page.text = text_new_new[0] + ''.join(header + body for header, body in text_new_new[1]) + text_new_new[2]
+
 			pywikibot.showDiff(text_old, page.text)
 			input('[press enter to continue]')
 			page.save('/* Translingual */ init')
-
-		#input('[press enter to continue]')
+		done.add(z)
 	except Exception as e:
 		print(page.text)
 		print(e)
