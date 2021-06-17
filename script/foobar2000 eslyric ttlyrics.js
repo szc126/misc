2ded8071d3ec7763ad0fbbce1e19564599fe2745
szc126 <29127915+szc126@users.noreply.github.com>
2021-06-17 02:22:31 -0700

    Update foobar2000 eslyric ttlyrics.js

--- script/foobar2000 eslyric ttlyrics.js
+++ script/foobar2000 eslyric ttlyrics.js
@@ -1,11 +1,14 @@
-// based on ttplayer 0.0.3.js by ohyeah
+// modified from ttplayer 0.0.3.js by ohyeah
 
-function get_my_name() {
+function get_my_name(sub) {
+	if (sub) {
+		return "TTLyrics" + ": " + sub + "|千千歌词" + "：" + sub;
+	}
 	return "TTLyrics|千千歌词";
 }
 
 function get_version() {
-	return "1";
+	return "2";
 }
 
 function get_author() {
@@ -13,6 +16,7 @@ function get_author() {
 }
 
 var SERVER = "http://lyrics.ttlyrics.com:86";
+var LRC_MAX = 3;
 
 function start_search(info, callback) {
 	var http_client = utils.CreateHttpClient();
@@ -22,10 +26,10 @@ function start_search(info, callback) {
 	var title = info.Title;
 	var results = [];
 
-	url = SERVER + "/api/search/?title=" + process_keywords(title) + "&artist=" + process_keywords(artist);
+	url = SERVER + "/api/search/?title=" + encodeURIComponent(process_keywords(title)) + "&artist=" + encodeURIComponent(process_keywords(artist));
 	var xml_text = http_client.Request(url);
 	if (http_client.StatusCode != 200) {
-		dbg_trace("request url[" + url + "] error : " + http_client.StatusCode);
+		log(http_client.StatusCode + ": " + url);
 		return;
 	}
 
@@ -33,7 +37,7 @@ function start_search(info, callback) {
 	try {
 		xmlDoc = new ActiveXObject("Msxml2.DOMDocument");
 	} catch (e) {
-		dbg_trace("create object 'MSXML.DOMDocument' error : " + e.message);
+		log("create object 'MSXML.DOMDocument' error: " + e.message);
 		return;
 	}
 
@@ -41,45 +45,51 @@ function start_search(info, callback) {
 
 	xmlDoc.loadXML(xml_text);
 	var lyrics = xmlDoc.getElementsByTagName("lrc");
-	for (var i = 0; i < lyrics.length; i++) {
+	log(lyrics[0].getAttribute("artist") + "／" + (lyrics.length - 1) + "个→" + LRC_MAX + "个");
+	for (var i = 1; i < lyrics.length; i++) {
 		try {
 			results.push({
-				title: lyrics[i].getAttribute("title"),
-				artist: lyrics[i].getAttribute("artist"),
+				//title: lyrics[i].getAttribute("title"),
+				//artist: lyrics[i].getAttribute("artist").slice(lyrics[i].getAttribute("artist").indexOf("]") + 1),
+				// circumvent ESLyric's arcane match filtering
+				title: title,
+				artist: artist,
+				source: lyrics[i].getAttribute("artist").slice(1, lyrics[i].getAttribute("artist").indexOf("]")),
 				id: lyrics[i].getAttribute("id")
 			});
 		} catch (e) {
-			dbg_trace(e.message);
+			log(e.message);
 			continue;
 		}
 	}
 
-	for (var i = 0; i < results.length; i++) {
+	var lrc_success_n = 0;
+	for (var i = 0; lrc_success_n < LRC_MAX; i++) {
 		if (callback.IsAborting()) {
-			dbg_trace("user aborted");
+			log("user aborted");
 			break;
 		}
 		url = SERVER + "/api/download/?id=" + results[i].id;
 		var lyric_text = http_client.Request(url);
 		if (http_client.StatusCode != 200) {
-			dbg_trace("request url[" + url + "] error : " + http_client.StatusCode);
+			log(http_client.StatusCode + ": " + url);
 			continue;
 		}
-		//add lyric to eslyric
+		// add lyric to ESLyric
 		_new_lyric.Title = results[i].title;
 		_new_lyric.Artist = results[i].artist;
-		_new_lyric.Source = get_my_name();
+		_new_lyric.Source = get_my_name(results[i].source);
 		_new_lyric.LyricText = lyric_text;
 		_new_lyric.Location = url;
 		callback.AddLyric(_new_lyric);
 		if (i % 2 == 0) callback.Refresh();
+		lrc_success_n++;
 	}
 
 	_new_lyric.Dispose();
-
 }
 
-function dbg_trace(s) {
+function log(s) {
 	fb.trace("TTLyrics: " + s);
 }
 
@@ -93,6 +103,6 @@ function process_keywords(str) {
 	s = s.replace(/\(.*?\)|\[.*?]|{.*?}|（.*?/g, "");
 	s = s.replace(/[-/:-@[-`{-~]+/g, "");
 	s = s.replace(/[\u2014\u2018\u201c\u2026\u3001\u3002\u300a\u300b\u300e\u300f\u3010\u3011\u30fb\uff01\uff08\uff09\uff0c\uff1a\uff1b\uff1f\uff5e\uffe5]+/g, "");
-	//fb.trace(s);
+	//log(s);
 	return s;
 }
