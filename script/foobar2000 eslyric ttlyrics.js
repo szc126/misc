baee2d6a2471ff27952602e17cb49ac3b9721af6
szc126 <29127915+szc126@users.noreply.github.com>
2021-06-17 22:15:13 -0700

    Update foobar2000 eslyric ttlyrics.js

--- script/foobar2000 eslyric ttlyrics.js
+++ script/foobar2000 eslyric ttlyrics.js
@@ -8,7 +8,7 @@ function get_my_name(sub_a, sub_b) {
 }
 
 function get_version() {
-	return "5";
+	return "6";
 }
 
 function get_author() {
@@ -16,90 +16,101 @@ function get_author() {
 }
 
 var SERVER = "http://lyrics.ttlyrics.com:86";
-var LRC_MAX = 3;
+var SKIP = "咪咕 新千千";
+var HTML_ENTITY_UN = {
+	"amp": "&",
+	"apos": "\'",
+}
 
 function start_search(info, callback) {
 	var http_client = utils.CreateHttpClient();
-	var url;
-
-	var artist = info.Artist;
-	var title = info.Title;
-	var results = [];
-
-	url = SERVER + "/api/search/?title=" + encodeURIComponent(title) + "&artist=" + encodeURIComponent(artist);
-	var xml_text = http_client.Request(url);
-	if (http_client.StatusCode != 200) {
-		log(http_client.StatusCode + ": " + url);
-		return;
-	}
-
 	var xmlDoc;
 	try {
 		xmlDoc = new ActiveXObject("Msxml2.DOMDocument");
 	} catch (e) {
-		log("create object 'MSXML.DOMDocument' error: " + e.message);
+		log("[ERROR]" + "Msxml2.DOMDocument: " + e.message);
+		return;
+	}
+	var _new_lyric = callback.CreateLyric();
+
+	var artist = info.Artist;
+	var title = info.Title;
+	var url;
+
+	url = SERVER + "/api/search/?title=" + encodeURIComponent(title) + "&artist=" + encodeURIComponent(artist);
+	log(url);
+	var xml_search = http_client.Request(url);
+	xmlDoc.loadXML(xml_search);
+	if (http_client.StatusCode != 200) {
+		var errmsg = xmlDoc.getElementsByTagName("result")[0].getAttribute("errmsg");
+		log("[ERROR]" + http_client.StatusCode + " " + errmsg);
 		return;
 	}
 
-	var _new_lyric = callback.CreateLyric();
+	var tags_lrc = xmlDoc.getElementsByTagName("lrc");
+	log(tags_lrc[0].getAttribute("artist") + "／" + (tags_lrc.length - 1) + "个");
+	for (var i = 1; i < tags_lrc.length; i++) {
+		var lrc_meta = {
+			title: tags_lrc[i].getAttribute("title"),
+			artist: tags_lrc[i].getAttribute("artist").slice(tags_lrc[i].getAttribute("artist").indexOf("]") + 1),
+			album: tags_lrc[i].getAttribute("album"),
+			source: tags_lrc[i].getAttribute("artist").slice(1, tags_lrc[i].getAttribute("artist").indexOf("]")),
+			id: tags_lrc[i].getAttribute("id")
+		}
+		log(lrc_meta.source + "／" + lrc_meta.artist + "／" + lrc_meta.title);
 
-	xmlDoc.loadXML(xml_text);
-	var lyrics = xmlDoc.getElementsByTagName("lrc");
-	log(lyrics[0].getAttribute("artist") + "／" + (lyrics.length - 1) + "个→" + LRC_MAX + "个");
-	for (var i = 1; i < lyrics.length; i++) {
-		try {
-			results.push({
-				//title: lyrics[i].getAttribute("title"),
-				//artist: lyrics[i].getAttribute("artist").slice(lyrics[i].getAttribute("artist").indexOf("]") + 1),
-				// circumvent ESLyric's arcane match filtering
-				title: title,
-				artist: artist,
-				source: lyrics[i].getAttribute("artist").slice(1, lyrics[i].getAttribute("artist").indexOf("]")),
-				id: lyrics[i].getAttribute("id")
-			});
-		} catch (e) {
-			log(e.message);
+		if (SKIP.indexOf(lrc_meta.source) > -1) {
+			log("    " + "Skipped");
 			continue;
 		}
-	}
 
-	var lrc_success_n = 0;
-	for (var i = 0; lrc_success_n < LRC_MAX; i++) {
 		if (callback.IsAborting()) {
-			log("user aborted");
+			log("User aborted");
 			break;
 		}
-		url = SERVER + "/api/download/?id=" + results[i].id;
-		var lyric_text = http_client.Request(url);
+
+		url = SERVER + "/api/download/?id=" + lrc_meta.id;
+		log("    " + url);
+		var lrc = http_client.Request(url);
 		if (http_client.StatusCode != 200) {
-			log(http_client.StatusCode + ": " + url);
+			xmlDoc.loadXML(lrc);
+			var errmsg = xmlDoc.getElementsByTagName("result")[0].getAttribute("errmsg");
+			log("    [ERROR]" + http_client.StatusCode + " " + errmsg);
 			continue;
 		}
-		// add lyric to ESLyric
-		if (lyric_text.indexOf("     ") > 0) {
-			_new_lyric.Title = results[i].title;
-			_new_lyric.Artist = results[i].artist;
-			_new_lyric.Source = get_my_name(results[i].source + ": no CHN", results[i].source + "／单语");
-			_new_lyric.LyricText = lyric_text.replace(/     .+/g, "");
+
+		lrc = lrc.replace(/&(\w+);/g, function(_, k) {
+			return HTML_ENTITY_UN[k];
+		});
+
+		if (
+			lrc.indexOf("暂无歌词") > 0 ||
+			lrc.indexOf("此歌曲为纯音乐，请欣赏") > 0
+		) {
+			// no-op
+		} else if (lrc.indexOf("     ") > 0) {
+			_new_lyric.Title = lrc_meta.title;
+			_new_lyric.Artist = lrc_meta.artist;
+			_new_lyric.Source = get_my_name(lrc_meta.source + ": no CHN", lrc_meta.source + "／单语");
+			_new_lyric.LyricText = lrc.replace(/     .+/g, "");
 			_new_lyric.Location = url;
 			callback.AddLyric(_new_lyric);
 
-			_new_lyric.Title = results[i].title;
-			_new_lyric.Artist = results[i].artist;
-			_new_lyric.Source = get_my_name(results[i].source + ": with CHN", results[i].source + "／双语");
-			_new_lyric.LyricText = lyric_text;
+			_new_lyric.Title = lrc_meta.title;
+			_new_lyric.Artist = lrc_meta.artist;
+			_new_lyric.Source = get_my_name(lrc_meta.source + ": with CHN", lrc_meta.source + "／双语");
+			_new_lyric.LyricText = lrc;
 			_new_lyric.Location = url;
 			callback.AddLyric(_new_lyric);
 		} else {
-			_new_lyric.Title = results[i].title;
-			_new_lyric.Artist = results[i].artist;
-			_new_lyric.Source = get_my_name(results[i].source);
-			_new_lyric.LyricText = lyric_text;
+			_new_lyric.Title = lrc_meta.title;
+			_new_lyric.Artist = lrc_meta.artist;
+			_new_lyric.Source = get_my_name(lrc_meta.source);
+			_new_lyric.LyricText = lrc;
 			_new_lyric.Location = url;
 			callback.AddLyric(_new_lyric);
 		}
 		if (i % 2 == 0) callback.Refresh();
-		lrc_success_n++;
 	}
 
 	_new_lyric.Dispose();
