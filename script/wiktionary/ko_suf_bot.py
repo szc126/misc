401f1d7a2db35db2eb69b2ebdd99c2c700fdfcf5
szc <>
2021-08-16 22:27:38 -0700

    ko_suf_bot.py: update

--- script/wiktionary/ko_suf_bot.py
+++ script/wiktionary/ko_suf_bot.py
@@ -5,15 +5,21 @@ import re
 
 import subprocess
 
-suffix = '를|을|는|은|에|의|로|으로|이|가'
+suffix = '이|를|을|는|은|에|의|으로|까지|에서|부터|께|께서|만|들|며|에는'
+suffix_other = '이다|요' # these are special-cased below. this is just for notes
 replaced = []
 ignore = ['consultant']
 
 site = pywikibot.Site()
 gen = site.search('insource:/\]\[\[(' + suffix + ')\]\]/')
 
-def doer(match):
-	d = match.group(1) + '🧡' + match.group(2) + match.group(3) + match.group(4)
+def doer_3(match):
+	d = match.group(1) + match.group(2).replace('[[', '[[🧡') + match.group(3)
+	replaced.append(d)
+	return d
+
+def doer_2(match):
+	d = match.group(1) + match.group(2).replace('[[', '[[🧡').replace('|', '|🧡')
 	replaced.append(d)
 	return d
 
@@ -26,25 +32,59 @@ for page in gen:
 		replaced = []
 		text_old = page.text
 
-		# do twice: {{t|ko|[[손바닥]][[을]] [[얼굴]][[에]] [[대다]]}}
+		# one or more links to suffixes,
+		# preceded by
+		# linked Hangul, bold Hangul, or pure Hangul, and
+		# followed by
+		# a space + other stuff that made sense during adjustment
+		# NOTE: pipe only for links, else it also matches
+		# {{uxi|ko|[[이]]
 		page.text = re.sub(
-			r"((?:\[\[[가-힣]+\]\]|'''[가-힣]+'''|[가-힣]+)\[\[)(" + suffix + r")(\]\])( [^\s{}]*?(?:\]\]|''')[|} .,!?]| [^\s{}]*?[} .,!?])",
-			doer,
+			r"(\[\[[가-힣🧡|-]+\]\]|'''[가-힣🧡-]+'''|[가-힣🧡-]+)((?:\[\[(?:" + suffix + r")\]\])+)( [^\s{}]*?(?:\]\]|''')[|} .,!?]| [^\s{}]*?[} .,!?])",
+			doer_3,
 			page.text,
 		)
+		# do twice
+		# [[손바닥]][[을]] [[얼굴]][[에]] [[대다]]
 		page.text = re.sub(
-			r"((?:\[\[[가-힣]+\]\]|'''[가-힣]+'''|[가-힣]+)\[\[)(" + suffix + r")(\]\])( [^\s{}]*?(?:\]\]|''')[|} .,!?]| [^\s{}]*?[} .,!?])",
-			doer,
+			r"(\[\[[가-힣🧡|-]+\]\]|'''[가-힣🧡-]+'''|[가-힣🧡-]+)((?:\[\[(?:" + suffix + r")\]\])+)( [^\s{}]*?(?:\]\]|''')[|} .,!?]| [^\s{}]*?[} .,!?])",
+			doer_3,
 			page.text,
 		)
-		summary = suffix + '：' + '◆'.join(replaced).replace("'''", "")
+		# sentence final
+		# [[앞]][[에서]][[요]].
+		# [[사람]][[이다|이에]][[요]].
+		page.text = re.sub(
+			r"(\[\[[가-힣🧡|-]+\]\]|'''[가-힣🧡-]+'''|[가-힣🧡-]+)(\[\[요\]\])",
+			doer_2,
+			page.text,
+		)
+		# 이다 inflexion
+		# [[것]][[이다|인]][[데]]
+		page.text = re.sub(
+			r"(\[\[[가-힣🧡|-]+\]\]|'''[가-힣🧡-]+'''|[가-힣🧡-]+)(\[\[이다\|[가-힣]+\]\])",
+			doer_2,
+			page.text,
+		)
+
+		summary = '◆'.join(replaced).replace("'''", "")
+		# delink non-additions.
+		# cram more nonsense in the summary,
+		# produce less html in the rendered summary,
+		# and highlight the remaining additions
+		summary = re.sub(
+			r'(\[\[(?:[^\]]+\|)?([^]]+)\]\])',
+			lambda match: match.group(2) if (not '🧡' in match.group(2)) else match.group(1),
+			summary,
+		)
 		print(summary.replace('🧡', '-'))
 
 		pywikibot.showDiff(text_old, page.text)
 		page.text = page.text.replace('🧡', '-') # 🧡 because the normal hyphen has extremely low visibility in the diff
 
 		if text_old != page.text:
-			reply = input('[press enter to continue, x enter to cancel]')
+			#reply = input('[press enter to continue, x enter to cancel]')
+			reply = 'x'
 
 			if reply == 'x':
 				pass
