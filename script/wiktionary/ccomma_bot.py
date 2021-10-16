085fa36062d75b087663576b5342b375410a38e5
szc <>
2021-10-15 22:55:31 -0700

    ccomma_bot.py: repurpose into a general find-and-replace script

--- script/wiktionary/ccomma_bot.py
+++ script/wiktionary/ccomma_bot.py
@@ -4,33 +4,36 @@ from pywikibot import pagegenerators
 import re
 
 site = pywikibot.Site()
-gen = site.search('hastemplate:zh-pron "cantonese lemmas" insource:/\|c=[^|}=]+, /')
+gen = site.search('korean insource:/[가-힣] \([㐀-龥]/')
+
+replaced = []
+
+def doer(match):
+	d = re.sub(r'([가-힣 ]+) \(([㐀-龥]+)\)', r'\1(\2)', match.group(0))
+	replaced.append(d)
+	return d
 
 for page in gen:
-	if '——' in page.title():
-		continue
-
-	if '，' in page.title():
-		continue
-
 	print(page.title())
 	try:
 		text_old = page.text
+		replaced = []
 		page.text = re.sub(
-			r'\|c=.+',
-			lambda match: re.sub(r'\s*,\s*', r',', match.group(0)),
+			r'{{[^|]+\|ko[^}]*[가-힣 ]+ \([㐀-龥]+\)[^}]*}}',
+			doer,
 			page.text,
 		)
 		pywikibot.showDiff(text_old, page.text)
 
 		if text_old != page.text:
-			reply = input('[press enter to continue, x enter to cancel]')
+			#reply = input('[press enter to continue, x enter to cancel]')
+			reply = ''
 
 			if reply == 'x':
 				pass
 				print('Skipped.')
 			else:
-				page.save('{{zh-pron}} Cantonese: remove improper spaces surrounding commas')
+				page.save('Korean: remove space before parens: ' + ', '.join(set(replaced)))
 				print('Saved.')
 	except Exception as e:
 		print(page.text)
