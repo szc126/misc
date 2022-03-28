c94e1f3617f64312e826674fe1912cc5f0b12f56
szc <>
2022-03-28 03:20:51 -0700

    ccomma_bot.py: {{internationalism}}

--- script/wiktionary/ccomma_bot.py
+++ script/wiktionary/ccomma_bot.py
@@ -6,7 +6,7 @@ import re
 site = pywikibot.Site()
 #page = pywikibot.Page(site, 'Template:Song box')
 #gen = page.getReferences(page)
-gen = site.search(' "japanese katakana" "japanese nouns" -etymology -"alternative form of" -hastemplate:ja-see -hastemplate:ja-def -"short for" -"clipping of"   -"alternative spelling of" -hastemplate:elements -"japanese proper nouns" -intitle:/ー/ -insource:/head=\[\[/ -hastemplate:ja-kanjitab ')
+gen = site.search(' "japanese katakana" "japanese nouns" -etymology -insource:/((alternative|alt) (form|spelling|sp)|short for|clip)|ja-(def|see)|\{\{elements|head=\[\[/ -"japanese proper nouns" -intitle:/ー/ -insource:/ja-noun\|[^}]+ / -hastemplate:ja-kanjitab -hastemplate:"U:ja:biology" ')
 
 replaced = []
 
@@ -16,14 +16,16 @@ def doer(match):
 	replaced.append(match.group(2) + '→' + a[2])
 	return ''.join(a)
 
-ffwd = 0
+bool_skip = True
 for page in gen:
-	if ffwd == 0:
+	"""
+	if bool_skip:
 		print(page.title())
-		if page.title()[0] == 'ア':
-			ffwd = 1
+		if page.title()[0] == 'ス': # start from this point in the list instead of from the top
+			bool_skip = False
 		else:
 			continue
+	"""
 
 	if 'Etymology' in page.text:
 		continue
@@ -31,44 +33,54 @@ for page in gen:
 	print(page.title())
 	try:
 		text_old = page.text
-		replaced = []
-		#page.text = re.sub(r'(\|date.+)({{date.+)', doer, page.text, flags = re.I)
+		lang_code = ''
+		while not lang_code == 'next':
+			replaced = []
+			#page.text = re.sub(r'(\|date.+)({{date.+)', doer, page.text, flags = re.I)
 
-		candidates = re.search(r'\[\[([^\]:]+)\]\]', page.text)
-		if not candidates:
-			candidates = re.search(r'# *([a-z]+)', page.text)
-		if not candidates:
-			continue
-		ety = 'From {{bor|ja|en|' + candidates.group(1) + '}}.'
-		replaced.append(ety)
-
-		print('\thttps://en.wiktionary.org/wiki/' + page.title())
-		print('\thttps://www.weblio.jp/content/' + page.title())
-		print('\thttps://en.wiktionary.org/wiki/' + candidates.group(1))
-
-		temp = page.text.split('===')
-		if "Alternative forms" in page.text:
-			temp.insert(3, 'Etymology')
-			temp.insert(4, '\n' + ety + '\n\n')
-		else:
-			temp.insert(1, 'Etymology')
-			temp.insert(2, '\n' + ety + '\n\n')
-		page.text = '==='.join(temp)
-
-		if text_old != page.text:
-			summary = 'add Etymology: ' + ', '.join(set(replaced))
-			print('\t' + summary)
-			#pywikibot.showDiff(text_old, page.text)
-			print(page.text)
-			reply = input('[press enter to continue, x enter to cancel]')
-			#reply = ''
-
-			if reply == 'x':
-				pass
-				print('Skipped.')
+			candidates = re.search(r'\[\[([^\]:]+)\]\]', page.text)
+			if not candidates:
+				candidates = re.search(r'# *([a-z]+)', page.text)
+			if not candidates:
+				lang_code = 'next'
+				continue
+			if lang_code == '':
+				ety = '{{internationalism|ja}}; see {{cog|en|' + candidates.group(1) + '}}.'
 			else:
-				page.save(summary)
-				print('Saved.')
+				ety = '{{bor+|ja|' + lang_code + '|' + candidates.group(1) + '}}.'
+			replaced.append(ety)
+
+			print('\thttps://en.wiktionary.org/wiki/' + page.title())
+			print('\thttps://www.weblio.jp/content/' + page.title())
+			print('\thttps://en.wiktionary.org/wiki/' + candidates.group(1).replace(' ', '_'))
+
+			temp = page.text.split('===')
+			if "Alternative forms" in page.text:
+				temp.insert(3, 'Etymology')
+				temp.insert(4, '\n' + ety + '\n\n')
+			else:
+				temp.insert(1, 'Etymology')
+				temp.insert(2, '\n' + ety + '\n\n')
+			page.text = '==='.join(temp)
+
+			if text_old != page.text:
+				summary = 'add Ety: ' + ', '.join(set(replaced))
+				print('\t' + summary)
+				#pywikibot.showDiff(text_old, page.text)
+				print(page.text)
+				reply = input('[press enter to continue, x enter to cancel, langcode enter to change]')
+
+				if reply == '':
+					lang_code = 'next'
+					page.save(summary)
+					print('Saved.')
+				elif reply == 'x':
+					lang_code = 'next'
+					print('Skipped.')
+				else:
+					lang_code = reply
+					page.text = text_old
+					print('Trying again.')
 	except Exception as e:
 		print(page.text)
 		print(e)
