5eaecaaf5f37065190c49672bc75518b6df4053b
szc <>
2023-03-01 04:39:23 -0800

    foobar2000 eslyric (0.5.x) vocadb.js: update

--- script/foobar2000 eslyric (0.5.x) vocadb.js
+++ script/foobar2000 eslyric (0.5.x) vocadb.js
@@ -1,31 +1,37 @@
 // 記得把繁簡轉換關掉喎
-// remember to turn off Trad. Ch. to Simp. Ch. conversion
+// remember to turn off Trad. Chn. to Simp. Chn. conversion
+// 繁体字→簡体字変換の無効化もお忘れなく
 // Tools > ESLyric > 高級搜索設置
 // Tools > ESLyric > Search Settings
 
 export function getConfig(cfg) {
 	cfg.name = 'VocaDB';
-	cfg.version = '2023.02.27';
-	cfg.author = 'hoshou wa ato nannen?';
+	cfg.version = '2023.03.01';
+	cfg.author = 'transgender queen boudica';
 	cfg.useRawMeta = false;
 }
 
 let SERVERS = [
-	'http://vocadb.net',
-	'http://utaitedb.net'
+	'https://vocadb.net',
+	'https://utaitedb.net',
 ];
 
 let SERVERS_NAMES = [
 	'VocaDB',
-	'UtaiteDB'
+	'UtaiteDB',
 ];
 
-let wtf = true; // XXX: HttpSendRequestA error - [0x0000013D]
-
 export function getLyrics(meta, man) {
+	// 相關文件夾外則取消搜索
 	// skip songs that aren't in my vocal synth folder
+	// 特定フォルダー外のものを無視
+	//
+	// ☞ 量體裁衣
+	// ☞ modify as needed
+	// ☞ 自身の状況に応じて修正をしましょう
+	//
 	if (meta.path.indexOf('ボカロUTAU') == -1) {
-		log('範圍外，已跳過');
+		console.log('範圍外，已跳過／Skipped: Out of scope');
 		return;
 	}
 
@@ -33,39 +39,37 @@ export function getLyrics(meta, man) {
 		let headers = {
 			'Accept': 'application/json',
 			'User-Agent': 'VocaDB for ESLyric for foobar2000',
-		}
+		};
 		let url = SERVERS[i_server] + '/api/songs?query=' + encodeURIComponent(meta.title);
-		url += '&songTypes=Original,Mashup&maxResults=3&preferAccurateMatches=true&nameMatchMode=Exact&fields=Lyrics';
+		url += '&maxResults=5&preferAccurateMatches=true&nameMatchMode=Exact&fields=Lyrics';
 		url += '&advancedFilters[0][description]=Lyrics%3A+Any+language&advancedFilters[0][filterType]=Lyrics&advancedFilters[0][negate]=false&advancedFilters[0][param]=*';
-		if (wtf) url = 'http://api.allorigins.win/get?url=' + encodeURIComponent(url);
 		//console.log(url);
 
 		request({
 			url: url,
 			headers: headers,
 		}, (err, res, body) => {
-			if (!err && res.statusCode == 200) {
-				let response = JSON.parse(body);
-				if (wtf) response = JSON.parse(response['contents']);
-				for (var i_item = 0; i_item < response['items'].length; i_item++) {
-					var item = response['items'][i_item];
+			if (err || res.statusCode != 200) {
+				return;
+			}
 
-					console.log(SERVERS[i_server] + '/S/' + item['id']);
-					console.log(item['artistString'] + '／' + item['name']);
-
-					for (var i_lyric = 0; i_lyric < item['lyrics'].length; i_lyric++) {
-						let lyricMeta = man.createLyric();
-						lyricMeta.title = item['name'];
-						lyricMeta.artist = item['artistString'];
-						// XXX: new ESLyric cannot write source?
-						lyricMeta.source = SERVERS_NAMES[i_server];
-						lyricMeta.source += ': ' + item['lyrics'][i_lyric]['translationType']
-						if (item['lyrics'][i_lyric]['cultureCode'] != '') lyricMeta.source += ': ' + item['lyrics'][i_lyric]['cultureCode'];
-						// XXX
-						lyricMeta.album = '(' + lyricMeta.source + ')';
-						lyricMeta.lyricText = item['lyrics'][i_lyric]['value'];
-						man.addLyric(lyricMeta);
-					}
+			body = JSON.parse(body);
+			for (var i_item = 0; i_item < body['items'].length; i_item++) {
+				var item = body['items'][i_item];
+				//console.log(SERVERS[i_server] + '/S/' + item['id']);
+				//console.log(item['artistString'] + '／' + item['name']);
+				for (var i_lyric = 0; i_lyric < item['lyrics'].length; i_lyric++) {
+					let lyricMeta = man.createLyric();
+					lyricMeta.title = item['name'];
+					lyricMeta.artist = item['artistString'];
+					// XXX: new ESLyric cannot write source?
+					lyricMeta.source = SERVERS_NAMES[i_server];
+					lyricMeta.source += ': ' + item['lyrics'][i_lyric]['translationType']
+					if (item['lyrics'][i_lyric]['cultureCode'] != '') lyricMeta.source += ': ' + item['lyrics'][i_lyric]['cultureCode'];
+					// XXX
+					lyricMeta.album = '(' + lyricMeta.source + ')';
+					lyricMeta.lyricText = item['lyrics'][i_lyric]['value'];
+					man.addLyric(lyricMeta);
 				}
 			}
 		});
