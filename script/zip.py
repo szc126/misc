6fa287da493daf0c68e9277578b14749fcd0ae5e
szc126 <29127915+szc126@users.noreply.github.com>
2024-03-16 05:31:03 -0700

    script/zip.py: ask for count and vary behavior

--- script/zip.py
+++ script/zip.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 
-lines = [[], []]
+import sys
+
+lines = [ [] for _ in range(int(sys.argv[1])) ]
 set = 0
 while True:
 	line = input()
@@ -16,8 +18,18 @@ for i in range(len(lines)):
 	while len(lines[i]) < max_len:
 		lines[i].append('')
 
-lines = list(zip(*lines))
-lines = ['\n'.join(line) for line in lines]
-lines = '\n'.join(lines)
+if len(lines) == 4:
+	a = zip(lines[0], lines[1])
+	b = zip(lines[2], lines[3])
 
-print(lines)
+	a = [';' + yue + ':' + cmn for yue, cmn in a]
+	b = [';' + yue + ':' + cmn for yue, cmn in b]
+
+	print('\n'.join('\n'.join(ab) for ab in zip(a, b)))
+
+if len(lines) == 2:
+	lines = list(zip(*lines))
+	lines = ['\n'.join(line) for line in lines]
+	lines = '\n'.join(lines)
+
+	print(lines)
